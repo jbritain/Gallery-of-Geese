ab2dd0ce94acfa6fb48df66710c59ef26f11ef28
Pr0x1mas <50422789+Pr0x1mas@users.noreply.github.com>
2021-07-05 23:20:22 +0100

    stuff

---
+++ about.md
@@ -0,0 +1,13 @@
+---
+layout: default
+title: About
+permalink: /about
+---
+
+# About the Gallery of Geese
+
+## Why?
+My good friend [The Goose Lord](https://github.com/TheProgramableTurtle) has a bit of an obsession with geese. One fine day, he had the idea to create a website known as "The Gallery of Geese". The idea was good, but sadly his webdev knowledge was not. Skip forward several months, I'm on holiday, and bored. With nothing better to do, I took out my laptop, wrote some code, and with the help of somebody with a long name, started compiling some epic images of geese.
+
+## How?
+[Look at the code yourself](https://github.com/Pr0x1mas/Gallery-of-Geese)
---
+++ js/gooseGrid.js
@@ -0,0 +1,65 @@
+var geeseContainer;
+
+function createGooseGrid(){
+    geeseContainer = document.getElementById("geeseContainer");
+
+    geese = getGeese().geese;
+    
+    geese.forEach(goose => {
+        addGoose(goose.name, goose.description, goose.filename);
+    });
+
+    var today = new Date();  
+    gooseOfTheDay = geese[Math.floor(seededRandom(today.getFullYear()+(today.getMonth()+1)+today.getDate()) * geese.length)];
+
+    document.getElementById("gotdName").innerHTML = gooseOfTheDay.name;
+    document.getElementById("gotdDescription").innerHTML = gooseOfTheDay.description;
+    document.getElementById("gotdImg").src = "/assets/geese/" + gooseOfTheDay.filename;
+
+}
+
+function getGeese(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", '/assets/geese/geese.json', false);
+    xmlhttp.send();
+
+    if(xmlhttp.status == 200) {
+        result = xmlhttp.responseText;
+        return JSON.parse(result);
+    }
+}
+
+function addGoose(name, description, filename){newGoose = document.createElement("div");
+    newGoose = document.createElement("div");
+    newGooseText = document.createElement("div");
+
+    newGoose.id = name;
+    newGoose.classList.add("gooseCard");
+
+    gooseImage = document.createElement("img");
+    gooseName = document.createElement("h2");
+    gooseDescription = document.createElement("p");
+
+    gooseImage.src = "/assets/geese/" + filename;
+    gooseName.innerHTML = name;
+    gooseDescription.innerHTML = description;
+
+    //newGoose.appendChild(gooseName);
+    //newGoose.appendChild(gooseDescription);
+
+    //newGoose.style.backgroundImage = "url('" + "/assets/geese/" + filename + "')"
+
+    newGooseText.appendChild(gooseName);
+    newGooseText.appendChild(gooseDescription);
+
+    newGoose.appendChild(gooseImage);
+    newGoose.appendChild(newGooseText);
+
+
+    geeseContainer.appendChild(newGoose);
+}
+
+function seededRandom(seed) { //https://stackoverflow.com/a/19303725
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+}
